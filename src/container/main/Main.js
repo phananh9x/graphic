6e605eeb522a7e85dba1198316b8024fd5a31340
Anh Phan <anh.phan@innomizetech.com>
2019-06-01 10:27:45 +0700

    Refactor codes

--- src/container/main/Main.js
+++ src/container/main/Main.js
@@ -257,29 +257,29 @@ class Main extends Component {
         const ctx = this.myRef.current.getContext("2d");
         let a = Math.sqrt(2.0) / 2;
         let x1, x2, x3, x4, x5, x6, x7, x8, y1, y2, y3, y4, y5, y6, y7, y8;
-        x1 = Draw.convertCoordinateX(parseInt((x + cd / 2) - (y + cr / 2) * a));
-        y1 = Draw.convertCoordinateY(parseInt(z - (y + cr / 2) * a));
+        x1 = Draw.convertCoordinateX(parseInt((x + cd / 2) - (z + cr / 2) * a));
+        y1 = Draw.convertCoordinateY(parseInt(y - (z + cr / 2) * a));
 
-        x2 = Draw.convertCoordinateX(parseInt((x - cd / 2) - (y + cr / 2) * a));
-        y2 = Draw.convertCoordinateY(parseInt(z - (y + cr / 2) * a));
+        x2 = Draw.convertCoordinateX(parseInt((x - cd / 2) - (z + cr / 2) * a));
+        y2 = Draw.convertCoordinateY(parseInt(y - (z + cr / 2) * a));
 
-        x3 = Draw.convertCoordinateX(parseInt((x - cd / 2) - (y - cr / 2) * a));
-        y3 = Draw.convertCoordinateY(parseInt(z - (y - cr / 2) * a));
+        x3 = Draw.convertCoordinateX(parseInt((x - cd / 2) - (z - cr / 2) * a));
+        y3 = Draw.convertCoordinateY(parseInt(y - (z - cr / 2) * a));
 
-        x4 = Draw.convertCoordinateX(parseInt((x + cd / 2) - (y - cr / 2) * a));
-        y4 = Draw.convertCoordinateY(parseInt(z - (y - cr / 2) * a));
+        x4 = Draw.convertCoordinateX(parseInt((x + cd / 2) - (z - cr / 2) * a));
+        y4 = Draw.convertCoordinateY(parseInt(y - (z - cr / 2) * a));
         //////////////////////////////
-        x5 = Draw.convertCoordinateX(parseInt((x + cd / 2) - (y + cr / 2) * a));
-        y5 = Draw.convertCoordinateY(parseInt(z + cc - (y + cr / 2) * a));
+        x5 = Draw.convertCoordinateX(parseInt((x + cd / 2) - (z + cr / 2) * a));
+        y5 = Draw.convertCoordinateY(parseInt(y + cc - (z + cr / 2) * a));
 
-        x6 = Draw.convertCoordinateX(parseInt((x - cd / 2) - (y + cr / 2) * a));
-        y6 = Draw.convertCoordinateY(parseInt(z + cc - (y + cr / 2) * a));
+        x6 = Draw.convertCoordinateX(parseInt((x - cd / 2) - (z + cr / 2) * a));
+        y6 = Draw.convertCoordinateY(parseInt(y + cc - (z + cr / 2) * a));
 
-        x7 = Draw.convertCoordinateX(parseInt((x - cd / 2) - (y - cr / 2) * a));
-        y7 = Draw.convertCoordinateY(parseInt(z + cc - (y - cr / 2) * a));
+        x7 = Draw.convertCoordinateX(parseInt((x - cd / 2) - (z - cr / 2) * a));
+        y7 = Draw.convertCoordinateY(parseInt(y + cc - (z - cr / 2) * a));
 
-        x8 = Draw.convertCoordinateX(parseInt((x + cd / 2) - (y - cr / 2) * a));
-        y8 = Draw.convertCoordinateY(parseInt(z + cc - (y - cr / 2) * a));
+        x8 = Draw.convertCoordinateX(parseInt((x + cd / 2) - (z - cr / 2) * a));
+        y8 = Draw.convertCoordinateY(parseInt(y + cc - (z - cr / 2) * a));
 
         Draw.dda(ctx, x1, y1, x2, y2);
         Draw.ddaDashed(ctx, x2, y2, x3, y3);
@@ -293,14 +293,14 @@ class Main extends Component {
         Draw.dda(ctx, x2, y2, x6, y6);
         Draw.ddaDashed(ctx, x3, y3, x7, y7);
         Draw.dda(ctx, x4, y4, x8, y8);
-        Draw.drawText(ctx, x1, Draw.convertCoordinateY(parseInt(z - (y + cr / 2) * a) - 5), 'A');
-        Draw.drawText(ctx, x2, Draw.convertCoordinateY(parseInt(z - (y + cr / 2) * a) - 5), 'B');
-        Draw.drawText(ctx, x3, Draw.convertCoordinateY(parseInt(z - (y - cr / 2) * a) - 5), 'C');
-        Draw.drawText(ctx, x4, Draw.convertCoordinateY(parseInt(z - (y - cr / 2) * a) - 5), 'D');
-        Draw.drawText(ctx, x5, Draw.convertCoordinateY(parseInt(z + cc - (y + cr / 2) * a) + 1), 'E');
-        Draw.drawText(ctx, x6, Draw.convertCoordinateY(parseInt(z + cc - (y + cr / 2) * a) + 1), 'F');
-        Draw.drawText(ctx, x7, Draw.convertCoordinateY(parseInt(z + cc - (y - cr / 2) * a) + 1), 'G');
-        Draw.drawText(ctx, x8, Draw.convertCoordinateY(parseInt(z + cc - (y - cr / 2) * a) + 1), 'H');
+        Draw.drawText(ctx, x1, Draw.convertCoordinateY(parseInt(y - (z + cr / 2) * a) - 5), 'A');
+        Draw.drawText(ctx, x2, Draw.convertCoordinateY(parseInt(y - (z + cr / 2) * a) - 5), 'B');
+        Draw.drawText(ctx, x3, Draw.convertCoordinateY(parseInt(y - (z - cr / 2) * a) - 5), 'C');
+        Draw.drawText(ctx, x4, Draw.convertCoordinateY(parseInt(y - (z - cr / 2) * a) - 5), 'D');
+        Draw.drawText(ctx, x5, Draw.convertCoordinateY(parseInt(y + cc - (z + cr / 2) * a) + 1), 'E');
+        Draw.drawText(ctx, x6, Draw.convertCoordinateY(parseInt(y + cc - (z + cr / 2) * a) + 1), 'F');
+        Draw.drawText(ctx, x7, Draw.convertCoordinateY(parseInt(y + cc - (z - cr / 2) * a) + 1), 'G');
+        Draw.drawText(ctx, x8, Draw.convertCoordinateY(parseInt(y + cc - (z - cr / 2) * a) + 1), 'H');
         this.setState({
             rect: {
                 ...this.state.rect,
@@ -374,7 +374,7 @@ class Main extends Component {
                 </div>
                 <div className="col-7">
                     <p className="font-weight-bold">Màn hình vẽ</p>
-                    <p>{active !== 2 ? 'y' : 'z'}</p>
+                    <p>y</p>
                     <canvas ref={this.myRef} id="myCanvas" width="700" height="700"
                         style={{ border: '1px solid #c3c3c3' }}>
                         Your browser does not support the canvas element.</canvas>
@@ -387,7 +387,7 @@ class Main extends Component {
                         position: 'absolute',
                         top: '755px',
                         left: '75px'
-                    }}>{active === 2 ? 'y' : ''}</p>
+                    }}>{active === 2 ? 'z' : ''}</p>
                 </div>
                 <div className="col-2">
                     <p className="font-weight-bold">Thông tin chi tiết</p>
